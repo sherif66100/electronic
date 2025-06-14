56f810e42ccbf27bf7987e11bdbfcd996c380f02
sherif66100 <sherif.omar2323@gmail.com>
2025-06-14 14:27:43 +0300

    first upload of kahropidia

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿console.log("مرحباً بك في موقعي الشخصي!");
+function sendWhatsApp() {
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var fullMessage =
+        "الاسم: " + name + "%0A" +
+        "رقم الهاتف: " + phone + "%0A" +
+        "الرسالة: " + message;
+
+    var url = "https://wa.me/96551014962?text=" + fullMessage;
+
+    window.open(url, '_blank');
+}
